8021bd5c5f1537f04587a8063a388d800aee064a
yeongseo <2108melody@gmail.com>
2023-09-25 10:32:52 +0900

    20230925
    C(주문하기) 기능 러프 구현
    (메뉴 개수 고려한 코드 작성 필요)

--- routes/orders.js
+++ routes/orders.js
@@ -14,6 +14,33 @@ router.post('/', (req, res) => {
     const email = body.email;
 }) */
 
+router.get('/:uid/:mkid', (req, res) => {
+    console.log(req.params.uid);
+    console.log(req.params.mkid);
+    console.log(req.query.menus); // ["aa","bb"]
+    console.log(req.query.price);
+
+    uid=req.params.uid;
+    mkid=req.params.mkid;
+    price=req.query.price;
+    menus=JSON.parse(req.query.menus); // aa,bb
+    console.log(menus);
+
+    
+    for(i=0; i<req.query.menus.length; i++) { // 사용자 주문
+        req.query.menus[0];
+    }
+    oid=Math.floor(Math.random() * 1000001);
+    var sql = `insert into orders values (${oid}, json_object('list', json_array('${menus[0]}','${menus[1]}')), ${price}, default, 0, 'ing', '${uid}', ${mkid})`;
+    conn.query(sql, (err, rows) => {
+        if(err) {
+            throw err;
+        }
+        console.log(rows);
+        res.send(rows);
+    });
+});
+
 router.get('/:id/:type', (req, res) => {
     console.log(req.params.id);
     console.log(req.params.type);
