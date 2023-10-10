1761cf7223262507ed3805c716da09b07701791c
yeongseo <2108melody@gmail.com>
2023-10-10 20:58:05 +0900

    231010
    port 번호 변경

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var orderRouter = require('./routes/orders');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3306);
 
 app.use('/user', userRouter);
 app.use('/market', marketRouter);
