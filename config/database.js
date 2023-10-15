4d4c3a28e7bc4daf0543c86560c2936a2c509f1a
yeongseo <2108melody@gmail.com>
2023-10-15 20:38:29 +0900

    20231015
    AWS RDS 사용자, 비밀번호, DB 변경

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: 'maks-database.cqfnpacnwfir.ap-northeast-2.rds.amazonaws.com',
-    user: 'maks',
-    password: '2071049',
-    database: 'roy'
+    user: 'melody2108',
+    password: '162071049',
+    database: 'maksDB'
 };
