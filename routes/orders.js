5c6b6a460d933dea79f7c38e2d2c600acd6555c3
yeongseo <2108melody@gmail.com>
2023-12-08 20:32:49 +0900

    20231208
    example3

--- routes/orders.js
+++ routes/orders.js
@@ -18,8 +18,10 @@ router.get('/:uid/:mkid', (req, res) => {
     for (i=0; i<req.query.ids.length; i++) {
         if (i=0) {
             const json = `{"${req.query.ids[0]}": ${req.query.counts[0]}}`;
+            console.log(json);
         } else {
             json[`${req.query.ids[i]}`] = `${req.query.counts[i]}`;
+            console.log(json);
         }
     }
     
